5afd23e39edc9a36659f5f662071b005da3f501b
kennywhy@zohomail.com <Kw#72021569829>
2023-08-26 20:02:19 +0800

    批量重命名文件

---
+++ io_rename_file.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :io_rename_file.py
+# @Time      :8/26/2023 18:55
+# @Author    :Kenny
+# 批量文件重命名，重命名当前目录下的文件
+
+
+# coding=gbk
+import os
+import re
+import sys
+
+
+def add_mark():
+    pre = input("请输入需要添加的前缀:")
+    mark = "[%s]" % pre
+    old_names = os.listdir()
+    for old_name in old_names:
+        if old_name != sys.argv[0]:
+            os.rename(old_name, mark + old_name)
+
+
+def remove_mark():
+    old_names = os.listdir()
+    for old_name in old_names:
+        try:
+            result = re.match(r"(^\[.*\])(.*)", old_name).group(2)
+            rm = old_name
+
+            if result:
+                os.rename(old_name, result)
+            print("已为%s移除前缀" % rm)
+        except Exception as e:
+            pass
+
+
+def main():
+    while True:
+        option = int(input("请选择功能数值:\n1.添加前缀\n2.删除前缀\n3.退出程序\n"))
+        if option == 1:
+            add_mark()
+        elif option == 2:
+            remove_mark()
+        else:
+            exit()
+
+
+if __name__ == "__main__":
+    main()
